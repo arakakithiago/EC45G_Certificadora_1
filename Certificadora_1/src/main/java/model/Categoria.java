023abb05f12379efc9d47a2b937b2af0cf511c94
Thiago Arakaki <thiagoarakaki@alunos.utfpr.edu.br>
2025-11-10 18:36:26 -0300

    Criação de MVC
    Getters and setters, Jframes iniciais, e estrutura base do mvc

---
+++ Certificadora_1/src/main/java/model/Categoria.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+//import java.awt.Color;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Categoria {
+    private int id;
+    private String nome;
+    //private Color cor;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+}
---
+++ Certificadora_1/src/main/java/model/Tarefa.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Tarefa {
+    private int id;
+    private String nome;
+    private Date datahora;
+    private boolean concluida;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getDatahora() {
+        return datahora;
+    }
+
+    public void setDatahora(Date datahora) {
+        this.datahora = datahora;
+    }
+
+    public boolean isConcluida() {
+        return concluida;
+    }
+
+    public void setConcluida(boolean concluida) {
+        this.concluida = concluida;
+    }
+}
